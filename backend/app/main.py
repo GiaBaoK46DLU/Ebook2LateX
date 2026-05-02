7a5596a679a5631a791c2c22279a93f7fd5835df
Dinh Lam Gia Bao <2212343@dlu.edu.vn>
2026-05-02 09:30:17 +0700

    feat: cai dat FastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,11 @@
+# Gọi thư viện FastAPI
 from fastapi import FastAPI
 
-app = FastAPI(title="Ebook2LateX API")
+# Tạo đối tượng app từ class FastAPI
+app = FastAPI()
 
+# Tạo decorator cho app.get("/") 
+# Khi có người dùng truy cập vào trang chủ (web root), hàm này sẽ chạy
 @app.get("/")
 def read_root():
-    return {"message": "Backend Ebook2LateX đã chạy thành công!"}
+    return {"message": "Chao mung ban den voi Ebook2LateX!"}
